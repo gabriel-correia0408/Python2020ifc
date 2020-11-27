7016ad5c43e2fcdfd80d52ec4ef13f83d9081ec2
GabrielCorreia <gabrielmal38@gmail.com.br>
2020-11-26 22:47:14 -0300

    tentativa de adicionar post , para após fazer o get, dos dados, falhada esta tentativa , erro que aplicação não se encontra

--- app/app.py
+++ app/app.py
@@ -1,16 +1,20 @@
-# importações
 import os
 
 from flask import Flask
-from flask_cors import CORS  # permitir back receber json do front
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-# flask
 app = Flask(__name__)
-CORS(app)  # aplicar o cross domain
+CORS(app)
 # sqlalchemy com sqlite
 path = os.path.dirname(os.path.abspath(__file__))
-arquivobd = os.path.join(path, 'pessoas.db')
+arquivobd = os.path.join(path, 'hoteis.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + arquivobd
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # remover warnings
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    from sql_alchemy import banco
+
+    banco.init_app(app)
+    app.run(debug=True)
--- app/domains/hoteis/view.py
+++ app/domains/hoteis/view.py
@@ -1,6 +1,10 @@
-from app.config import *
 from flask import request, jsonify
-from app.domains.hoteis.actions import get as get_hoteis
+
+from app.app import *
+from app.domains.hoteis.actions import get as get_hoteis, create as create_hoteis
+from app.domains.hoteis.schema import HoteisSchema
+
+_HOTEIS_SCHEMA = HoteisSchema
 
 
 @app.route("/")
@@ -8,8 +12,15 @@ def inicio():
     return "Bem vindo ao backend"
 
 
-@app.route("/hoteis", methods=['GET'])
+@app.route("/buscarhoteis", methods=['GET'])
 def get():
     query = request.args
     hoteis = get_hoteis(query)
-    return jsonify
+    return jsonify(_HOTEIS_SCHEMA.dump(hoteis))
+
+
+@app.route("/postarhoteis", methods=['POST'])
+def post():
+    payload = request.get_json()
+    hoteis = create_hoteis(payload)
+    return _HOTEIS_SCHEMA.dump(hoteis), 201
